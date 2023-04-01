e7246a8210f253bdbcc9d99fe4b1f313140e7be8
manar <faresjehad44@gmail.com>
2023-04-01 16:18:01 +0300

    first commit

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Support\Facades\DB;
+use App\Models\Task;
+use Illuminate\Http\Request;
+use League\CommonMark\Extension\Table\Table;
+use Spatie\LaravelIgnition\Http\Requests\UpdateConfigRequest;
+
+class TaskController extends Controller
+{
+
+
+    public function store(Request $request){
+        // dd($request);
+        DB::table('tasks')->insert([
+            'name' =>$request->name,
+            'created_at' => now(),
+            'updated_at'=>now()
+        ]);
+        return redirect()->back();
+    }
+    public function index (){
+        $tasks = DB::table('tasks')->get();
+        return view('index',compact('tasks'));
+    }
+    public function update(Request $request, $id)
+    {
+        $task = DB::table('tasks')->find($id);
+        $task->name = $request->input('name');
+        $task->update();
+
+       return redirect('')->with('Updated successfully!');
+    }
+    public function destroy($id){
+        DB::table('tasks')->where ('id',$id )->delete();
+        return redirect()->back();
+
+    }
+    public function edit($id){
+        $tasks = DB::table('tasks')->where('id', '=', $id)->find($id);
+        return view('edit', compact('tasks'));
+
+    }
+}
+
---
+++ routes/web.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\DB;
+Use App\Http\Controllers\TaskController;
+use App\Models\Task;
+
+
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/',[TaskController::class, 'index']);
+Route::delete('delete/{id}',[TaskController::class, 'destroy'])->name('task.delete');
+Route::post('edit/{id}',[TaskController::class, 'edit']);
+Route::put('update/{id}',[TaskController::class, 'update']);
+Route::post('insert/{id}',[TaskController::class, 'store'])->name('task.insert');
+
+
+
+
